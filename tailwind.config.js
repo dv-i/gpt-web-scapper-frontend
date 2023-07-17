dab778175cf32837c247794825c724ed9a064cd2
Bhavya Shah - dvi <dvi.builders@gmail.com>
2023-07-17 19:52:22 -0400

    made the frontend

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+// eslint-disable-next-line no-undef
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+			},
+			colors:{
+				primary: '#0d9488',
+				secondary: '#ffffff'
+			}
+		},
+	},
+	plugins: [],
+};
