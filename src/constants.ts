be0e8ba0d7252168ec5f0c9d181190720ae421ef
Jaimish Ashar <jaimish97@gmail.com>
2023-07-25 00:42:28 -0400

    Removes extra slash from base url

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 // export const BASE_API_URL = "http://localhost:3000";
 export const BASE_API_URL =
-	"https://scrapergpt-backend-b088310f8268.herokuapp.com/";
+	"https://scrapergpt-backend-b088310f8268.herokuapp.com";
